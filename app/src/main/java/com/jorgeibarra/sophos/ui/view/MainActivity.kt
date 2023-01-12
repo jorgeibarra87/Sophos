d28f1372a2008dd7da0a74e9435d03e05762f7da
jorgeibarra87 <jorgeibarra87@gmail.com>
2023-01-12 17:29:16 -0500

    add changes for menu main

--- app/src/main/java/com/jorgeibarra/sophos/ui/view/MainActivity.kt
+++ app/src/main/java/com/jorgeibarra/sophos/ui/view/MainActivity.kt
@@ -1,25 +1,65 @@
 package com.jorgeibarra.sophos.ui.view
 
+import android.content.res.Configuration
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import androidx.navigation.NavController
+import androidx.navigation.findNavController
 import androidx.navigation.fragment.NavHostFragment
 import androidx.navigation.fragment.findNavController
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.navigateUp
+import androidx.navigation.ui.setupActionBarWithNavController
 import com.jorgeibarra.sophos.R
 import com.jorgeibarra.sophos.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
 
-    lateinit var binding:ActivityMainBinding
-    private lateinit var navController: NavController
+    lateinit var appBarConfiguration: AppBarConfiguration
+    lateinit var binding: ActivityMainBinding
+    lateinit var navController: NavController
+
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
+        setTheme(R.style.Theme_Sophos)
         super.onCreate(savedInstanceState)
+
         binding = ActivityMainBinding.inflate(layoutInflater)
         val view = binding.root
         setContentView(view)
+
+        setSupportActionBar(binding.activitiyToolbar)
+
         val navHostFragment = supportFragmentManager.findFragmentById(R.id.NavHostFragment) as NavHostFragment
-        navController = navHostFragment.findNavController()
+        navController = navHostFragment.navController
+        appBarConfiguration = AppBarConfiguration(navController.graph)
+        setupActionBarWithNavController(navController, appBarConfiguration)
+
+
+        when(this.resources.configuration.uiMode.and(Configuration.UI_MODE_NIGHT_MASK)){
+            Configuration.UI_MODE_NIGHT_YES -> {
+                binding.activitiyToolbar.setTitleTextColor(resources.getColor(R.color.white))
+                binding.activitiyToolbar.overflowIcon = getDrawable(R.drawable.ic_menu)
+            }
+            Configuration.UI_MODE_NIGHT_NO -> {
+                binding.activitiyToolbar.setTitleTextColor(resources.getColor(R.color.primaryLightColor))
+                binding.activitiyToolbar.overflowIcon = getDrawable(R.drawable.ic_menu)
+            }
+            Configuration.UI_MODE_NIGHT_UNDEFINED -> {}
+
+        }
+
+        supportActionBar?.setDisplayHomeAsUpEnabled(true)
+        supportActionBar?.setHomeAsUpIndicator(R.drawable.ic_back_arrow)
+
+
+    }
+
+    override fun onSupportNavigateUp(): Boolean {
+        val navController = findNavController(R.id.NavHostFragment)
+        return navController.navigateUp(appBarConfiguration) || super.onSupportNavigateUp()
+
     }
 
 
